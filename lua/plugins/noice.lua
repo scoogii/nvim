17431db65140abf4fb9b5e919d5cc9ba96096d2d
scoogii <christian.nguyen6@outlook.com>
2023-10-03 02:22:47 +1100

    noice changes

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -19,19 +19,10 @@ return {
           input = {}, -- Used by input()
         },
       },
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
       -- you can enable a preset for easier configuration
       presets = {
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
       },
     })
   end
