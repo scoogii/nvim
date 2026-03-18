f825e8b003b60aee81c40f715d24d132e4171b1b
scoogii <christian.nguyen6@outlook.com>
2026-03-19 00:01:18 +1100

    .

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -14,7 +14,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'catppuccin',
+        theme = 'auto',
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
         disabled_filetypes = {
